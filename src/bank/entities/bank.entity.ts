cadce806fb4079ca85cfa9acabbcbdc9653e1a38
jangsen <dh7895210@naver.com>
2024-10-06 15:55:41 +0900

    Entity init

--- src/bank/entities/bank.entity.ts
+++ src/bank/entities/bank.entity.ts
@@ -1,1 +1,14 @@
-export class Bank {}
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+@Entity()
+export class Bank extends BaseEntity {
+  @Column()
+  public bankName: string;
+
+  @Column()
+  public user: string;
+
+  @Column()
+  public amount: number;
+}
