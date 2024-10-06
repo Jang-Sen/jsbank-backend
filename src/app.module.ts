25d31175e73276602d800d6a320001931ab707d9
jangsen <dh7895210@naver.com>
2024-10-06 15:38:12 +0900

    connect DB & docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { BankModule } from './bank/bank.module';
+import { DataBaseModule } from './db/db.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [BankModule],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3307,
+    //   username: 'root',
+    //   password: 'pass_js',
+    //   database: 'db_js',
+    //   entities: [],
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_USER: Joi.string().required(),
+        MYSQL_ROOT_PASSWORD: Joi.string().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+        MYSQL_PORT: Joi.number().required(),
+      }),
+      envFilePath: '.env', // .env 파일의 경로를 명시적으로 추가
+      isGlobal: true, // 모든 모듈에서 ConfigService 사용 가능하게 함
+    }),
+    BankModule,
+    DataBaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db/db.module.ts
@@ -0,0 +1,33 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      // type: 'mysql',
+      // host: 'localhost',
+      // port: 3307,
+      // username: 'root',
+      // password: 'pass_js',
+      // database: 'db_js',
+      // entities: [],
+      // autoLoadEntities: true,
+      // synchronize: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (c: ConfigService) => ({
+        type: 'mysql',
+        host: c.get('MYSQL_HOST'),
+        port: c.get('MYSQL_PORT'),
+        username: c.get('MYSQL_USER'),
+        password: c.get('MYSQL_ROOT_PASSWORD'),
+        database: c.get('MYSQL_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DataBaseModule {}
